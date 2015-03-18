ae472534684210441ad6c5878c7e391472da384d
Kevin Anderson <anderskev@gmail.com>
2015-03-18 00:53:32 -0400

    edit restaurant name

--- webserver.py
+++ webserver.py
@@ -21,10 +21,11 @@ class webServerHandler(BaseHTTPRequestHandler):
                 output = ""
                 output += "<html><body>"
                 output += "<h1>Restaurants</h1>"
+                output += "<a href=/restaurants/new>Make a new restaurant</a></br>"
                 restolist = session.query(Restaurant).all()
                 for resto in restolist:
                     output += ("</br>%s</br>" % resto.name)
-                    output += "<a href=#>Edit</a></br>"
+                    output += "<a href='/restaurants/%s/edit'>Edit</a></br>" % resto.id
                     output += "<a href=#>Delete</a>"
                 output += "</body></html>"
                 self.wfile.write(output)
@@ -38,7 +39,7 @@ class webServerHandler(BaseHTTPRequestHandler):
                 output = ""
                 output += "<html><body>"
                 output += "Add new Restaurant"
-                output += '''<form method='POST' enctype='multipart/form-data' action='/restaurants'>
+                output += '''<form method='POST' enctype='multipart/form-data' action='/restaurants/new'>
                             <h2>Name of restaurant to add:</h2><input name="name" type="text" >
                             <input type="submit" value="Submit"></form>'''
                 output += "</body></html"
@@ -46,33 +47,73 @@ class webServerHandler(BaseHTTPRequestHandler):
                 print output
                 return
 
+            if self.path.endswith("/edit"):
+                restaurantIDPath = self.path.split("/")[2]
+                myRestaurantQuery = session.query(Restaurant).filter_by(id=
+                    restaurantIDPath).one()
+                if myRestaurantQuery != []:
+                    self.send_response(200)
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+                output = "<html><body>"
+                output +="<h1>"
+                output += myRestaurantQuery.name
+                output += "</h1>"
+                output += '''<form method='POST' enctype='multipart/form-data'
+                            action='/restaurants/%s/edit'>''' % restaurantIDPath
+                output += '''<input name="newRestaurantName" type="text" 
+                            placeholder = '%s' >''' % myRestaurantQuery.name
+                output += "<input type='submit' value='Rename'></form>"
+                output += "</body></html>"
+                self.wfile.write(output)
+                print output
+
         except IOError:
             self.send_error(404, "File Not Found %s" % self.path)
 
     def do_POST(self):
         try:
-            self.send_response(301)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-            if ctype == 'multipart/form-data':
-                fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('name')
-            newResto = Restaurant(name = messagecontent[0])
-            session.add(newResto)
-            session.commit()
-            print "creating output"
-            output = ""
-            output += "<html><body>"
-            output += " <h2>Restaurant added!</h2>"
-            restolist = session.query(Restaurant).all()
-            for resto in restolist:
-                output += ("</br>%s</br>" % resto.name)
-                output += "<a href=#>Edit</a></br>"
-                output += "<a href=#>Delete</a>"
-            output += "</body></html>"
-            self.wfile.write(output)
-            print output
+            if self.path.endswith("/edit"):
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                messagecontent = fields.get('newRestaurantName')
+                restaurantIDPath = self.path.split("/")[2]
+
+                myRestaurantQuery = session.query(Restaurant).filter_by(id=
+                                    restaurantIDPath).one()
+                if myRestaurantQuery != []:
+                    myRestaurantQuery.name = messagecontent[0]
+                    session.add(myRestaurantQuery)
+                    session.commit()
+                self.send_response(301)
+                self.send_header('Content-type', 'text/html')
+                self.send_header('Location', '/restaurants')
+                self.end_headers()
+
+            if self.path.endswith("/restaurants/new"):
+                self.send_response(301)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('name')
+                newResto = Restaurant(name = messagecontent[0])
+                session.add(newResto)
+                session.commit()
+                print "creating output"
+                output = ""
+                output += "<html><body>"
+                output += " <h2>Restaurant added!</h2>"
+                restolist = session.query(Restaurant).all()
+                for resto in restolist:
+                    output += ("</br>%s</br>" % resto.name)
+                    output += "<a href=#>Edit</a></br>"
+                    output += "<a href=#>Delete</a>"
+                output += "</body></html>"
+                self.wfile.write(output)
+                print output
 
         except:
             pass
