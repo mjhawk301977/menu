310efa6063022a55a2d198e057ffd8d9f1642773
Kevin Anderson <anderskev@gmail.com>
2015-03-18 18:39:28 -0400

    add CSS and API endpoints

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect, flash
+from flask import Flask, render_template, url_for, request, redirect, flash, jsonify
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -10,6 +10,21 @@ Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+#API Endpoint (GET Request)
+@app.route('/restaurants/<int:restaurant_id>/menu/JSON')
+def restaurantMenuJSON(restaurant_id):
+	restaurant = session.query(Restaurant).filter_by(id = 
+		restaurant_id).one()
+	items = session.query(MenuItem).filter_by(restaurant_id = 
+		restaurant.id).all()
+	return jsonify(MenuItems=[i.serialize for i in items])
+
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/JSON')
+def menuItemJSON(restaurant_id, menu_id):
+	item = session.query(MenuItem).filter_by(id = 
+		menu_id).one()
+	return jsonify(item.serialize)
 
 @app.route('/')
 @app.route('/restaurants/<int:restaurant_id>/')
@@ -43,6 +58,12 @@ def editMenuItem(restaurant_id, menu_id):
 	if request.method == 'POST':
 		if request.form['name']:
 			editedItem.name = request.form['name']
+		if request.form['description']:
+			editedItem.description = request.form['name']
+		if request.form['price']:
+			editedItem.price = request.form['price']
+		if request.form['course']:
+			editedItem.course = request.form['course']
 		session.add(editedItem)
 		session.commit()
 		flash("menu item changed!")
